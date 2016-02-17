91638db6367a215dc5354df7f03a29f152dc4a22
Eric Zounes <eric.zounes@puppetlabs.com>
2016-02-17 13:19:50 -0800

    Support installing Splunk from a repo if package_source isn't specified.

--- manifests/init.pp
+++ manifests/init.pp
@@ -50,20 +50,22 @@ class splunk (
   $purge_inputs   = false,
   $purge_outputs  = false,
 ) inherits splunk::params {
-  include staging
 
   $virtual_service = $splunk::params::server_service
-  $staged_package  = staging_parse($package_source)
   $staging_subdir  = $splunk::params::staging_subdir
 
   $path_delimiter  = $splunk::params::path_delimiter
   $pkg_path_parts  = [$staging::path, $staging_subdir, $staged_package]
-  $pkg_source      = join($pkg_path_parts, $path_delimiter)
 
-  staging::file { $staged_package:
-    source => $package_source,
-    subdir => $staging_subdir,
-    before => Package[$package_name],
+  if $package_source != undef {
+    include staging
+    $staged_package  = staging_parse($package_source)
+    $pkg_source      = join($pkg_path_parts, $path_delimiter)
+    staging::file { $staged_package:
+      source => $package_source,
+      subdir => $staging_subdir,
+      before => Package[$package_name],
+    }
   }
 
   package { $package_name:
